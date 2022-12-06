38f0d5411e358c8a1644cf6441048c370c82a9b3
Carlos Diones <carlosdiones28@gmail.com>
2022-12-06 20:34:45 -0300

    creates interface to use with the AwesomeServer class

---
+++ AwesomeSauce.Api/IServer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AwesomeSauce
+{
+    interface IServer : IDisposable
+    {
+        IFeatureCollection Features { get; }
+        Task StartAsync<TContext>(IHttpApplication<TCOntext> application,
+            CancellationToken cancellationToken);
+        Task StopAsync(CancellationToken cancellationToken);
+    }
+}
