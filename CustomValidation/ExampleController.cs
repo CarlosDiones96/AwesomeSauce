0737a55b97806dcb2d045b2e03126e09a53a11ec
Carlos Diones <carlosdiones28@gmail.com>
2022-12-12 21:58:46 -0300

    adds CustomValidation example

---
+++ CustomValidation/ExampleController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CustomValidation
+{
+    [Route("api/[controller]")]
+    public class ExampleController : Controller
+    {
+        public IActionResult Post([FromBody] PersonDto item)
+        {
+            if (ModelState.IsValid)
+            {
+                return Ok();
+            }
+            else
+            {
+                return BadRequest(ModelState);
+            }
+        }
+    }
+}
