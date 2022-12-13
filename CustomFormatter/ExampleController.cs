d657ef2071e008bff2e0f7461b9640ac5c0fe360
Carlos Diones <carlosdiones28@gmail.com>
2022-12-13 15:21:30 -0300

    adds CustomFormatter example

---
+++ CustomFormatter/ExampleController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace CustomFormatter
+{
+    [Route("api/[controller]")]
+    public class ExampleController : Controller
+    {
+        public IActionResult Get()
+        {
+            IEnumerable<PersonDto> result = new PersonDto[]
+            {
+                new PersonDto {FirstName = "Fanie", LastName = "Reynders"},
+                new PersonDto {FirstName = "Joe", LastName = "Soap"}
+            };
+
+            return Ok(result);
+        }
+    }
+}
