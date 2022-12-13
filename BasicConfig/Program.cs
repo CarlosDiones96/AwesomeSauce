387dd996a3f43cbb69b651231a65bc9759ed8f0b
Carlos Diones <carlosdiones28@gmail.com>
2022-12-13 17:03:41 -0300

    adds BasicConfig to solution

---
+++ BasicConfig/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+
+namespace BasicConfig
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("awesomeConfig.json");
+            Configuration = builder.Build();
+
+            foreach (var item in Configuration.AsEnumerable())
+            {
+                Console.WriteLine($"Key: {item.Key}, Value: {item.Value}");
+            }
+
+            Console.ReadKey();
+        }
+
+        public static IConfigurationRoot Configuration { get; set; }
+    }
+}
