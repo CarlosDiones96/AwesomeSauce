36a4d522997f093acce10741be42572b32ae0d78
Carlos Diones <carlosdiones28@gmail.com>
2022-12-13 22:30:19 -0300

    adds AwesomeConfiguration example

---
+++ AwesomeConfiguration/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using Microsoft.Extensions.Configuration;
+using AwesomeConfiguration;
+
+namespace AwesomeConfiguration
+{
+    public class Program
+    {
+        public static IConfigurationRoot Configuration { get; set; }
+
+        static void Main(string[] args)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddLegacyXmlConfiguration("web.config");
+
+            Configuration = builder.Build();
+
+            foreach (var item in Configuration.AsEnumerable())
+            {
+                Console.WriteLine($"Key: {item.Key}, Value: {item.Value}");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
