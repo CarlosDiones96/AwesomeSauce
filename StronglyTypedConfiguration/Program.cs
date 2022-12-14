c5dc71415ae1d1e7e3cfb0a7801d5466078d9de8
Carlos Diones <carlosdiones28@gmail.com>
2022-12-14 16:32:54 -0300

    adds StrongyTypedConfiguration proj to solution

---
+++ StronglyTypedConfiguration/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using Microsoft.Extensions.Configuration;
+
+namespace StronglyTypedConfiguration
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("awesomeConfig.json");
+
+            var awesomeOptions = new AwesomeOptions();
+            builder.Build().Bind(awesomeOptions);
+
+            Console.WriteLine($"Foo: {awesomeOptions.Foo}");
+            Console.WriteLine($"Bar: {awesomeOptions.Bar}");
+            Console.WriteLine($"Baz.Foo: {awesomeOptions.Baz.Foo}");
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
