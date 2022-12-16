a3fc8aa8e1a0c2bb1cf0693804fccf4131bb09ba
Carlos Diones <carlosdiones28@gmail.com>
2022-12-16 13:44:03 -0300

    Adds LoggerFiltering example to solution

---
+++ LoggerFiltering/Program.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Console;
+
+namespace LoggerFiltering
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            new WebHostBuilder()
+                .UseKestrel()
+                .UseStartup<Startup>()
+                .ConfigureLogging(logging =>
+                {
+                    logging
+                        .SetMinimumLevel(LogLevel.None)
+                        .AddFilter("Default", LogLevel.Error)
+                        .AddFilter<ConsoleLoggerProvider>("Program.Startup", LogLevel.Critical);
+                })
+                .Build()
+                .Run();
+        }
+    }
+}
---
+++ LoggerFiltering/Startup.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace LoggerFiltering
+{
+    public class Startup
+    {
+        private readonly ILogger logger;
+
+        public Startup(ILogger<Startup> logger)
+        {
+            this.logger = logger;
+        }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            app.Run(async (context) =>
+            {
+                logger.LogInformation("Log entry 1");
+                logger.LogWarning("Log entry 2");
+                logger.LogError("Log entry 3");
+                logger.LogCritical("Log entry 4");
+
+                await context.Response.WriteAsync("Hello world");
+            });
+        }
+    }
+}
