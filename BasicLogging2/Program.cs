a790bbf09ebfbd869c703d02860b879fa44dc00d
Carlos Diones <carlosdiones28@gmail.com>
2022-12-15 22:33:58 -0300

    Adds BasicLogging2 example to solution

---
+++ BasicLogging2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace BasicLogging2
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            new WebHostBuilder()
+                .UseKestrel()
+                .UseStartup<Startup>()
+                .ConfigureLogging(logging =>
+               {
+                   logging.AddConsole();
+               })
+                .Build()
+                .Run();
+        }
+    }
+}
---
+++ BasicLogging2/Startup.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace BasicLogging2
+{
+    public class Startup
+    {
+        private readonly ILoggerFactory loggerFactory;
+
+        public Startup(ILoggerFactory loggerFactory)
+        {
+            this.loggerFactory = loggerFactory;
+            var constructorLogger = loggerFactory.CreateLogger("Startup.ctor");
+            constructorLogger.LogInformation("Logging from constructor!");
+        }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            var configureLogger = loggerFactory.CreateLogger("Startup.Configure");
+            configureLogger.LogInformation("Logging from Configure!");
+
+            app.Run(async (context) =>
+            {
+                var logger = loggerFactory.CreateLogger("Startup.Configure.Run");
+                configureLogger.LogInformation("Logging from app.Run!");
+                await context.Response.WriteAsync("Hello world");
+            });
+        }
+    }
+}
