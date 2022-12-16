03c61378f17b0e51a2a315a8ff229010a42288c8
Carlos Diones <carlosdiones28@gmail.com>
2022-12-16 15:45:33 -0300

    Adds Exceptions example to solution

---
+++ Exceptions/ExampleController.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Exceptions
+{
+    [Route("~/broken")]
+    public class ExampleController
+    {
+        public string Get(int id)
+        {
+            throw new ArgumentNullException();
+        }
+    }
+}
